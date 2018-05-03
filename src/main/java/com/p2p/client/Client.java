689f8336ccc21e26ea1a71bd0eaf6363a3ec75b7
abhiyu <abakre5@gmail.com>
2018-05-03 09:56:47 +0530

    Version 2.0 Host URL

--- src/main/java/com/p2p/client/Client.java
+++ src/main/java/com/p2p/client/Client.java
@@ -8,115 +8,144 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.Base64;
 
 import com.p2p.server.Server;
- 
+
 public class Client {
 
 	private Socket socket;
-	
+
 	private String filePath;
-	
+
 	public Client(String host, int port, String filePath) {
-		
+
 		this.filePath = filePath;
-		
+
 		try {
-			
+
 			this.socket = new Socket(host, port);
-		
+
 		} catch (UnknownHostException e) {
 
 			System.out.println("Error : Host not Found");
-			
-		} catch (IOException e) {
-			
-			System.out.println("Error : Port not Found");
-			
-		}
-		
-	}
-	
-	public void recieve() {
-		
-		File file = new File(filePath);
-        
-        ObjectOutputStream objectOutputStream = null;
- 
-        try {
-        	
-			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-			
+
 		} catch (IOException e) {
 
-        	System.out.println("Error : Error in init Output Stream");
-			
+			System.out.println("Error : Port not Found");
+
 		}
-        
-        try {
-        	
-			objectOutputStream.writeObject(file.getName());
-		
-        } catch (IOException e) {
-			
-			System.out.println("Error: Something went wrong");
-			
-		}
- 
-        FileInputStream fileInputStream = null;
-        
-        try {
-        	
-			fileInputStream = new FileInputStream(file);
-		
-        } catch (FileNotFoundException e) {
-        	
-			System.out.println("Error: Something went wrong");
-        	
-		}
-        
-        byte [] buffer = new byte[Server.BUFFER_SIZE];
-        
-        Integer bytesRead = 0;
- 
-        try {
-        	
-			while ((bytesRead = fileInputStream.read(buffer)) > 0) {
-			
-				objectOutputStream.writeObject(bytesRead);
-			    
-				objectOutputStream.writeObject(Arrays.copyOf(buffer, buffer.length));
-      
-			}
-			
-		} catch (IOException e) {
-			
-			System.out.println("Error: Download Fail");
-			
-		}
- 
-        try {
-			
-        	objectOutputStream.close();
-			
-	        objectOutputStream.close();
-	        
-	        socket.close();
-		
-        } catch (IOException e) {
-        	
-        	System.out.println("Error: Closing connection");
-        	
-		}
-        
-        System.exit(0);   
-		
+
 	}
-	
-    public static void main(String[] args) throws Exception {
-    	
-    	new Client("localhost", Integer.parseInt(args[0]), args[1]).recieve();
-    
-    }
- 
-}  
+
+	public void recieve() {
+
+		File file = new File(filePath);
+
+		ObjectOutputStream objectOutputStream = null;
+
+		try {
+
+			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+		} catch (IOException e) {
+
+			System.out.println("Error : Error in init Output Stream");
+
+		}
+
+		try {
+
+			objectOutputStream.writeObject(file.getName());
+
+		} catch (IOException e) {
+
+			System.out.println("Error: Something went wrong");
+
+		}
+
+		FileInputStream fileInputStream = null;
+
+		try {
+
+			fileInputStream = new FileInputStream(file);
+
+		} catch (FileNotFoundException e) {
+
+			System.out.println("Error: Something went wrong");
+
+		}
+
+		byte[] buffer = new byte[Server.BUFFER_SIZE];
+
+		Integer bytesRead = 0;
+
+		try {
+
+			while ((bytesRead = fileInputStream.read(buffer)) > 0) {
+
+				objectOutputStream.writeObject(bytesRead);
+
+				objectOutputStream.writeObject(Arrays.copyOf(buffer, buffer.length));
+
+			}
+
+		} catch (IOException e) {
+
+			System.out.println("Error: Download Fail");
+
+		}
+
+		try {
+
+			objectOutputStream.close();
+
+			objectOutputStream.close();
+
+			socket.close();
+
+		} catch (IOException e) {
+
+			System.out.println("Error: Closing connection");
+
+		}
+
+		System.exit(0);
+
+	}
+
+	public static String hostAddress(String hostAddress) {
+
+		byte[] bytes = Base64.getDecoder().decode(hostAddress);
+
+		return new String(bytes);
+
+	}
+
+	public static String getHost(String hostInetAddress) {
+
+		String[] parts = hostInetAddress.split(":");
+
+		parts = parts[0].split("/");
+
+		return parts[1];
+
+	}
+
+	public static int getPort(String hostInetAddress) {
+
+		String[] parts = hostInetAddress.split(":");
+
+		return Integer.parseInt(parts[1]);
+
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		String hostInetAddress = Client.hostAddress(args[0]);
+
+		new Client(Client.getHost(hostInetAddress), Client.getPort(hostInetAddress), args[1]).recieve();
+
+	}
+
+}
--- src/main/java/com/p2p/server/Server.java
+++ src/main/java/com/p2p/server/Server.java
@@ -1,41 +1,50 @@
 package com.p2p.server;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
- 
+import java.util.Base64;
+
 public class Server extends Thread {
-	
-    public static final int BUFFER_SIZE = 100;
-    
-    private ServerSocket serverSocket;
- 
-    private Socket clientSocket;
-    
-    public Server(int port) throws IOException {
-    	
-    	this.serverSocket = new ServerSocket(port);
-    	
-    	System.out.println("Server started at Port :: " + serverSocket.getLocalPort());
-    	
-    }
-    
-    public void listenClient() throws IOException {
-    	
-    	while(true){
-		
-    		clientSocket = serverSocket.accept();
-			
-    		new Thread((Runnable) new SessionConnect(serverSocket, clientSocket)).start();
-		
-    	}
-    	
-    }
-    
-  
-    public static void main(String[] args) throws IOException {
-    	
-        new Server(2424).listenClient();
-   
-    }
-}  
+
+	public static final int BUFFER_SIZE = 100;
+
+	private ServerSocket serverSocket;
+
+	private Socket clientSocket;
+
+	public Server(int port) throws IOException {
+
+		this.serverSocket = new ServerSocket(63454);
+
+		serverSocket.getInetAddress();
+
+		String hostAddress = InetAddress.getLocalHost().toString();
+
+		hostAddress += ":" + serverSocket.getLocalPort();
+
+		String bytesEncoded = Base64.getEncoder().encodeToString(hostAddress.getBytes());
+
+		System.out.println("Host Address :: " + bytesEncoded);
+
+	}
+
+	public void listenClient() throws IOException {
+
+		while (true) {
+
+			clientSocket = serverSocket.accept();
+
+			new Thread((Runnable) new SessionConnect(serverSocket, clientSocket)).start();
+
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		new Server(2424).listenClient();
+
+	}
+}
